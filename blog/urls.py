fa241a66df30a0891caa6027ecb2798ad5404e6f
daniilseliverstov <selivrstov91@internet.ru>
2025-12-06 20:10:28 +0300

    delete

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home_page, name='home_page'),
-    path('blogs/<int:blog_id>/', views.blog_detail, name='blog_detail'),
-]
--- silvpr/urls.py
+++ silvpr/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+handler404 = 'blog.views.page_not_found'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
 ]
